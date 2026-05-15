8b6c2e8f4352b6fcc15bded7caf9d1187d024812
YasaXF <githubyasa@gmail.com>
2026-05-15 11:08:34 +0700

    Refactor Admin class for course management

--- src/user/Admin.java
+++ src/user/Admin.java
@@ -2,23 +2,23 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package user;
 
-/**
- *
- * @author Girls
- */
+package user;
 public class Admin extends User{
+    
+    public Admin(String username, String password){
+        super(username, password);
+    }
     public void manageCourses(){
-        
+        System.out.println("Admin mengelola course.");
     }
     public void assignTier(){
-        
+        System.out.println("Admin menentukan tier akses.");
     }
     public void validateSubmission(){
-        
+        System.out.println("Admin memvalidasi submission.");
     }
     public void publishMaterial(){
-        
+        System.out.println("Admin mempublish materi.");
     }
 }
